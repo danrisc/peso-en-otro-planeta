dacffdac5938a5c1a46ae16ff1c72517b0148039
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-05-09 10:58:19 -0300

    cambios a javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
-alert("está vinculado");
 var g_tierra = 9.8;
-   var g_marte = 3.7;
-   var peso = prompt("Ingrese su peso");
-     var peso_final;
+var g_marte = 3.7;
+var peso = prompt("Ingrese su peso");
+var peso_final;
   peso_final = peso * g_marte / g_tierra;
    peso_final = parseInt(peso_final);
-    alert("Tu peso en Marte es igual a: " + peso_final)
+    alert("Tu peso en Marte es de: " + peso_final)
